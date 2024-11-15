360f3c81d33c259c115d72b06c6aa53141eaed9a
yungflashh <streaktv8@gmail.com>
2024-11-15 09:57:20 +0100

    addded guest Ministers number

--- src/components/MinisterData.jsx
+++ src/components/MinisterData.jsx
@@ -6,6 +6,7 @@ import segun from "../assets/jpg/segun.jpg"
 import seyi from "../assets/jpg/seyi.jpg"
 import danny from "../assets/jpg/danny.jpg"
 import ayodeji from "../assets/jpg/ayodeji.jpg"
+import ruth from "../assets/jpg/ruth.jpg"
 
 const MinisterData = () => {
   const ministerData = [
@@ -14,35 +15,49 @@ const MinisterData = () => {
     {
       name: 'OLUWASEYI',
       image: seyi,
-      bio: 'Omotosho Oluwaseyi is a gifted music minister and professional photographer dedicated to inspiring lives through her talents. Known for her heartfelt worship, she leads people into deeper encounters with God through her gospel music.Married to the talented gospel music celebrity Omotosho Oluwafemi, the crooner of Momo Riri Re, Oluwaseyi shares a beautiful partnership in ministry and music. Together, they impact lives with their passion for spreading God’s love.In addition to her music, Oluwaseyi is a skilled photographer, capturing memorable moments with creativity and excellence. She continues to glorify God and touch lives through her gifts in music and artistry..'
+      bio: 'Omotosho Oluwaseyi is a gifted music minister and professional photographer dedicated to inspiring lives through her talents. Known for her heartfelt worship, she leads people into deeper encounters with God through her gospel music.Married to the talented gospel music celebrity Omotosho Oluwafemi, the crooner of Momo Riri Re, Oluwaseyi shares a beautiful partnership in ministry and music. Together, they impact lives with their passion for spreading God’s love.In addition to her music, Oluwaseyi is a skilled photographer, capturing memorable moments with creativity and excellence. She continues to glorify God and touch lives through her gifts in music and artistry..',
+      whatsapp : "https://wa.me/2348145944142?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
+    },
+
+
+    {
+      name: 'RUTH ONOGHA',
+      image: ruth,
+      bio: "Ruth Onogha is a gifted worshipper and an inspiring presence in the world of gospel music. From a young age, she began singing in the house of God, where her passion for praise and worship flourished. Known for her calm demeanor and intelligence, Ruth is versatile in her musical abilities and deeply committed to her faith.As a respected leader, Ruth has successfully led a choir unit, motivating and inspiring countless individuals in their spiritual journeys. Her ministry has taken her around the globe, where she has had the honor of sharing the stage with renowned gospel artists, including Dare Melody, Adegbodu Twins, Ojodu Taiwo, Bunmi Akinaanu(Omije ojumi) to mention a fewCurrently, Ruth serves as the Mass Choir Leader for the 23rd season of 'Praise in Advance' at Tower of Victory Church. Her powerful voice and heartfelt worship have touched and blessed the lives of many, enabling her to create an atmosphere where people encounter the presence of God. Ruth Onogha continues to be a beacon of inspiration, encouraging others to 6y in God's presence through song and worship.",
+      whatsapp : "https://wa.me/2348105030031?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
     },
     {
       name: 'JUWON PRAISE',
       image: juwon,
-      bio: 'Juwon praise is a powerful praise leader and song minister, renowned for her energetic, spirit-filled worship that uplifts and inspires. With a deep love for God and a calling to lead others into His presence, she brings both passion and devotion to every gathering. A dedicated health practitioner, she embodies compassion and care in all she does.'
+      bio: 'Juwon praise is a powerful praise leader and song minister, renowned for her energetic, spirit-filled worship that uplifts and inspires. With a deep love for God and a calling to lead others into His presence, she brings both passion and devotion to every gathering. A dedicated health practitioner, she embodies compassion and care in all she does.',
+      whatsapp : "https://wa.me/2349162041430?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
     },
     {
       name: 'BELOVED (OGO AMI)',
       image: beloved,
-      bio: 'BELOVED OGOAMI is filled with sound and  called by God to lead his people into deep prophetic encounter with the Father. BELOVED OGOAMI is the visioneer of BELOVED MUSIC MINISTRY and BELOVED TEENS MINISTRY.She has released some powerful and inspirational songs which includes EMI MIMO DUN, KING OF GLORY, ERU RE BAMI and so on.She is a woman of God who believes in the word "GOD FIRST" From an early age, Ayodeji Sampraiz has carried a profound passion for music and ministry. Starting to sing in the Choir at the tender age of 8. Ayodeji Sampraiz began to develop his gift, growing in both talent and spiritual depth. During his fourth year at the University of Lagos, he served as the music director of his fellowship, leading worship and music with excellence. His leadership continued during his service year in Kebbi, Nigeria, where he was appointed as the music director of his zone.Currently, Ayodeji Sampraiz is a committed member of Celebration Church International, where he contributes actively by serving in the media department. A gifted songwriter, Ayodeji Sampraiz has written numerous songs, with five of them produced so far, including the well-known and impactful "Enibafere." Through his music, Ayodeji Sampraiz seeks to ignite the fire of God across the nations, ministering with authority and power, and proclaiming the name of Jesus to his generation.His ultimate goal is to inspire a deeper connection to God through worship and to create music that transforms lives by drawing people into the presence of God.'
+      bio: 'BELOVED OGOAMI is filled with sound and  called by God to lead his people into deep prophetic encounter with the Father. BELOVED OGOAMI is the visioneer of BELOVED MUSIC MINISTRY and BELOVED TEENS MINISTRY.She has released some powerful and inspirational songs which includes EMI MIMO DUN, KING OF GLORY, ERU RE BAMI and so on.She is a woman of God who believes in the word "GOD FIRST" From an early age, Ayodeji Sampraiz has carried a profound passion for music and ministry. Starting to sing in the Choir at the tender age of 8. Ayodeji Sampraiz began to develop his gift, growing in both talent and spiritual depth. During his fourth year at the University of Lagos, he served as the music director of his fellowship, leading worship and music with excellence. His leadership continued during his service year in Kebbi, Nigeria, where he was appointed as the music director of his zone.Currently, Ayodeji Sampraiz is a committed member of Celebration Church International, where he contributes actively by serving in the media department. A gifted songwriter, Ayodeji Sampraiz has written numerous songs, with five of them produced so far, including the well-known and impactful "Enibafere." Through his music, Ayodeji Sampraiz seeks to ignite the fire of God across the nations, ministering with authority and power, and proclaiming the name of Jesus to his generation.His ultimate goal is to inspire a deeper connection to God through worship and to create music that transforms lives by drawing people into the presence of God.',
+      whatsapp : "https://wa.me/2349073749144?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
     },
 
     {
         name: 'AYODEJI SAMPRAIZ',
         image: ayodeji,
-        bio: 'From an early age, Ayodeji Sampraiz has carried a profound passion for music and ministry. Starting to sing in the Choir at the tender age of 8. Ayodeji Sampraiz began to develop his gift, growing in both talent and spiritual depth. During his fourth year at the University of Lagos, he served as the music director of his fellowship, leading worship and music with excellence. His leadership continued during his service year in Kebbi, Nigeria, where he was appointed as the music director of his zone.Currently, Ayodeji Sampraiz is a committed member of Celebration Church International, where he contributes actively by serving in the media department. A gifted songwriter, Ayodeji Sampraiz has written numerous songs, with five of them produced so far, including the well-known and impactful "Enibafere." Through his music, Ayodeji Sampraiz seeks to ignite the fire of God across the nations, ministering with authority and power, and proclaiming the name of Jesus to his generation.His ultimate goal is to inspire a deeper connection to God through worship and to create music that transforms lives by drawing people into the presence of God.'
+        bio: 'From an early age, Ayodeji Sampraiz has carried a profound passion for music and ministry. Starting to sing in the Choir at the tender age of 8. Ayodeji Sampraiz began to develop his gift, growing in both talent and spiritual depth. During his fourth year at the University of Lagos, he served as the music director of his fellowship, leading worship and music with excellence. His leadership continued during his service year in Kebbi, Nigeria, where he was appointed as the music director of his zone.Currently, Ayodeji Sampraiz is a committed member of Celebration Church International, where he contributes actively by serving in the media department. A gifted songwriter, Ayodeji Sampraiz has written numerous songs, with five of them produced so far, including the well-known and impactful "Enibafere." Through his music, Ayodeji Sampraiz seeks to ignite the fire of God across the nations, ministering with authority and power, and proclaiming the name of Jesus to his generation.His ultimate goal is to inspire a deeper connection to God through worship and to create music that transforms lives by drawing people into the presence of God.',
+      whatsapp : "https://wa.me/2348176731467?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
       },
 
       {
         name: 'DANNYT',
         image: danny,
-        bio: 'Pastor Daniel Adenusi is a lover of God and good music, his exposure to music started at an early age and has been composing songs for many years . He is an incredible worshipper and energetic praiser and has contributed immensely to the development and progress of his local church music department, without mincing words he understoods how to make the best food that God likes whenever he enters into the Kitchen of Praise.'
+        bio: 'Pastor Daniel Adenusi is a lover of God and good music, his exposure to music started at an early age and has been composing songs for many years . He is an incredible worshipper and energetic praiser and has contributed immensely to the development and progress of his local church music department, without mincing words he understoods how to make the best food that God likes whenever he enters into the Kitchen of Praise.',
+      whatsapp : "https://wa.me/2349159710342?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
       },
 
       {
         name: 'SEGUN VICTORY',
         image: segun,
-        bio: 'I began my musical journey as a teenager in the choir at St. Peter Anglican Church in Shonyindo, Shagamu, Ogun State. Upon gaining admission to Lagos State University (LASU), I became a member of the choir at my current church, Tower of Victory Church International in Erunwen, Ikorodu, Lagos State. It was here that my career truly began to flourish. This is also where I adopted my stage name, "Segun Victory." Since then, my band and I have had the privilege of performing at various events, sharing God`s grace with the world. My goal for this year is to release my debut album. May God grant me the strength to accomplish this..'
+        bio: 'I began my musical journey as a teenager in the choir at St. Peter Anglican Church in Shonyindo, Shagamu, Ogun State. Upon gaining admission to Lagos State University (LASU), I became a member of the choir at my current church, Tower of Victory Church International in Erunwen, Ikorodu, Lagos State. It was here that my career truly began to flourish. This is also where I adopted my stage name, "Segun Victory." Since then, my band and I have had the privilege of performing at various events, sharing God`s grace with the world. My goal for this year is to release my debut album. May God grant me the strength to accomplish this..',
+      whatsapp : "https://wa.me/2348067521758?text=He%2C%20is%20able ,%20I%20got%20your%20Number%20from%20Praisee%20in%20Advance%3F"
       },
   ];
 
@@ -54,6 +69,7 @@ const MinisterData = () => {
           name={minister.name}
           image={minister.image}
           bio={minister.bio}
+          whatsapp={minister.whatsapp}
         />
       ))}
     </div>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,13 +1,14 @@
 
 import '../styles/ProfileCard.css'; // Import custom CSS for styling
 
-const ProfileCard = ({ name, image, bio }) => {
+const ProfileCard = ({ name, image, bio,whatsapp }) => {
   return (
     <div className="profile-card">
       <img className="profile-image" src={image} alt={name} />
       <div className="profile-info">
         <h2 className="profile-name">{name}</h2>
         <p className="profile-bio">{bio}</p>
+        <a href= {whatsapp}>  <button>   Get in touch  </button> </a>
       </div>
     </div>
   );
